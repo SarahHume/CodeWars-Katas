c8e6340c8b68d84edef27e36c38def399b9f4f80
SarahHume <sarahdawn.hume@gmail.com>
2023-09-26 14:38:23 +0100

    complete tested toCamelCase function

--- __tests__/toCamelCase.test.js
+++ __tests__/toCamelCase.test.js
@@ -2,8 +2,21 @@ const toCamelCase = require("../toCamelCase");
 
 describe("toCamelCase", () => {
 
-    test("", () => {
+    test("should return an empty string if passed an empty string", () => {
+        expect(toCamelCase("")).toBe("");
+    })
 
+    test("should return the same string if passed a non-empty string with no underscores or dashes", () => {
+        expect(toCamelCase("string")).toBe("string");
+    })
+
+    test("should return a different string if passed a non-empty string with underscores or dashes", () => {
+        expect(toCamelCase("not-this")).not.toBe("not-this");
+    })
+
+    test("should convert a passed string into camel case", () => {
+        expect(toCamelCase("this-is-right")).toBe("thisIsRight");
+        expect(toCamelCase("This_is_Right-too")).toBe("ThisIsRightToo");
     })
 
 })
--- toCamelCase.js
+++ toCamelCase.js
@@ -1,5 +1,20 @@
 function toCamelCase(str) {
 
+    let strArr = str.split("");
+
+    for (let i = 0; i < strArr.length; i++) {
+        if (strArr[i] === "-" || strArr[i] === "_") {
+            strArr[i + 1] = strArr[i + 1].toUpperCase();
+        }
+    }
+
+    const filtArr = strArr.filter((char) => char != "-" && char != "_");
+
+    let newStr = filtArr.join("");
+
+    console.log(newStr);
+
+    return newStr;
 }
 
 module.exports = toCamelCase;
