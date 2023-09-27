93d8f8f4a6b0eab28977c77248174e3fb862ea1c
SarahHume <sarahdawn.hume@gmail.com>
2023-09-27 14:46:42 +0100

    complete tested uniqueInOrder function

--- __tests__/uniqueInOrder.test.js
+++ __tests__/uniqueInOrder.test.js
@@ -2,8 +2,33 @@ const uniqueInOrder = require("../uniqueInOrder");
 
 describe("uniqueInOrder", () => {
 
-    test("", () => {
-        
+    test("should return an array", () => {
+        const string = "string";
+        const array = ["1", "2", "3", "4", "5"];
+        expect(Array.isArray(uniqueInOrder(string))).toBe(true);
+        expect(Array.isArray(uniqueInOrder(array))).toBe(true);
+    })
+
+    test("should return list of all elements if passed an iterable with no repeated elements", () => {
+        const string = "string";
+        const strExpected = ["s", "t", "r", "i", "n", "g"];
+        const array = ["1", "2", "3", "4", "5"];
+        expect(uniqueInOrder(string)).toEqual(strExpected);
+        expect(uniqueInOrder(array)).toEqual(array);
+    })
+
+    test("returned array should be unique", () => {
+        const array = ["1", "2", "3", "4", "5"];
+        expect(uniqueInOrder(array)).not.toBe(array);
+    })
+
+    test("should omit repeated elements", () => {
+        const string = "sstriiIngG"
+        const strExpected = ["s", "t", "r", "i", "I", "n", "g", "G"];
+        const array = [1, 1, "1", 12, "A", "A", 2, 3, "A", "a"];
+        const arrExpected = [1, "1", 12, "A", 2, 3, "A", "a"];
+        expect(uniqueInOrder(string)).toEqual(strExpected)
+        expect(uniqueInOrder(array)).toEqual(arrExpected);
     })
 
 })
--- uniqueInOrder.js
+++ uniqueInOrder.js
@@ -1,5 +1,15 @@
 function uniqueInOrder(iterable) {
 
+    const list = [];
+
+    for (let i = 0; i < iterable.length; i++) {
+        if (iterable[i] !== iterable[i+1]) {
+            list.push(iterable[i]);
+        }
+    }
+
+    return list;
+
 }
 
 module.exports = uniqueInOrder;
